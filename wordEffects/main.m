14ad25069f8bf4c4c50ac0f64cb17cc4b5a608f3
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-08-04 14:29:05 -0400

    Initial Commit

---
+++ wordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wordEffects
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-08-04.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
