3ae023004e2562837316b12685d89d9393c66a19
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-08-05 09:12:32 -0400

    Word effect complete, maybe implement combined word and command string manip

---
+++ wordEffects/InputHandler.h
@@ -0,0 +1,13 @@
+//
+//  inputHandler.h
+//  wordEffects
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-08-04.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface inputHandler : NSObject
+
+@end
---
+++ wordEffects/InputHandler.m
@@ -0,0 +1,13 @@
+//
+//  inputHandler.m
+//  wordEffects
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-08-04.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import "inputHandler.h"
+
+@implementation inputHandler
+
+@end
